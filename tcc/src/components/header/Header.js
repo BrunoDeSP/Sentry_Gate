0594649ef73112757a63b22c7cfd3a5312a3e916
BrunoDeSP <antunesbrunobarbosa1@gmail.com>
2022-05-25 10:51:10 -0300

    Primeiro commit

---
+++ tcc/src/components/header/Header.js
@@ -0,0 +1,46 @@
+import "./header.css";
+import { Link, useNavigate } from "react-router-dom";
+import { Popover } from "@headlessui/react";
+import { signOut } from "firebase/auth";
+import { auth } from "../data/firebase";
+
+export function Header() {
+  const navigate = useNavigate();
+
+  const logout = (e) => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((err) => alert(err.message));
+  };
+
+  return (
+    <div className="header">
+      <div className="logo">
+        <Link to="/">
+          <h1>
+            Sentry <span>Gate</span>
+          </h1>
+        </Link>
+      </div>
+      <div className="topics">
+        <Link to="/main" className="links">
+          Home
+        </Link>
+
+        <div className="relative">
+          <button>
+            <Link className="links" to='/about'>Sobre</Link>
+          </button>
+        </div>
+        <Link to="/" className="links">
+          Contato
+        </Link>
+        <button className="logoff" onClick={logout}>
+          Desconectar
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ tcc/src/route/RouteNavigation.js
@@ -0,0 +1,29 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { Header } from '../components/header/Header'
+import { Main } from "../components/mainScreen/main/Main";
+import { Form } from '../components/formScreen/Form';
+import { Register } from '../components/registerScreen/Register';
+import { VerifyEmail } from '../components/verifyScreen/VerifyEmail'
+import { Vision } from '../components/about/vision/Vision';
+import { Mission } from '../components/about/mission/Mission';
+import { About } from '../components/about/About';
+//import PrivateRoute from './PrivateRoute';
+
+export function RouteNavigation() {
+  return (
+      <>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Form />} /> 
+          <Route path="/main" element={<Main />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify-email" element={<VerifyEmail />} />
+          <Route path="/vision" element={<Vision />} />
+          <Route path="/mission" element={<Mission />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        {/* <Footer /> */}
+      </>
+  );
+}
